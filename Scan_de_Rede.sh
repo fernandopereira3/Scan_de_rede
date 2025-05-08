1fd484c91df432ac53bc23b51bafe730de84229b
PedroAssis501 <pedro.assis510@gmail.com>
2025-05-08 15:35:57 -0300

    Adicionado arquivo Scan

---
+++ Scan_de_Rede.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Verifica se está sendo executado como root
+if [ "$EUID" -ne 0 ]; then
+  echo "Por favor, execute como root (use sudo)."
+  exit 1
+fi
+
+# Solicita a faixa de IPs
+read -p "Digite a faixa de IP (ex: 192.168.1.0/24): " REDE
+
+# Realiza a varredura de descoberta de hosts
+nmap -sn "$REDE" -oG - | awk '/Up$/{print $2}' > ips_ativos.txt
+
+# Exibe cabeçalho
+echo ""
+echo "IP              | Status   | Hostname               | MAC (Fabricante)"
+echo "--------------------------------------------------------------------------"
+
+# Loop pelos IPs ativos
+while read ip; do
+    # Resolve o nome do host (DNS reverso)
+    nome=$(dig +short -x "$ip" | sed 's/\.$//')
+    [ -z "$nome" ] && nome=""
+
+    # Captura MAC + Fabricante do Nmap
+    mac_info=$(nmap -sn "$ip" | grep "MAC Address" | sed 's/MAC Address: //')
+    [ -z "$mac_info" ] && mac_info=""
+
+    # Exibe resultado formatado
+    printf "%-15s | %-8s | %-22s | %s\n" "$ip" "ONLINE" "$nome" "$mac_info"
+done < ips_ativos.txt
+
+# Remove arquivo temporário
+rm -f ips_ativos.txt
+
