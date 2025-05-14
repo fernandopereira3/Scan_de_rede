96271047536c7a81ce4f6421f3c2c8b22e68b260
Fernando Pereira <fernandopereira-3@hotmail.com>
2025-05-14 20:31:09 -0300

    Verificacao se NMAP esta instalado

--- scanner.sh
+++ scanner.sh
@@ -6,11 +6,18 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+#Verifica se o programa nmap está instalado
+if [ -e "/bin/nmap" ] ;then
+# Realiza a varredura de descoberta de hosts
+nmap -sn "$REDE" -oG - | awk '/Up$/{print $2}' > ips_ativos.txt
+else
+    echo "O programa nmap não está instalado. Por favor, instale-o."
+    exit 1
+fi
+
 # Solicita a faixa de IPs
 read -p "Digite a faixa de IP (ex: 192.168.1.0/24): " REDE
 
-# Realiza a varredura de descoberta de hosts
-nmap -sn "$REDE" -oG - | awk '/Up$/{print $2}' > ips_ativos.txt
 
 # Exibe cabeçalho
 echo ""
@@ -33,4 +40,3 @@ done < ips_ativos.txt
 
 # Remove arquivo temporário
 rm -f ips_ativos.txt
-
